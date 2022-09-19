2ec4917f22743ba0a92dcefe4b8d53356a668893
NazirLattimore0 <nlattim1@email.cpcc.edu>
2022-09-19 18:19:36 -0400

    first commit

---
+++ easy/easy.js
@@ -0,0 +1,11 @@
+var firstName = "Tristian";
+var secondName = "Josh";
+
+let firstNameLength = firstName.length;
+let secondNameLength = secondName.length;
+
+var characterAmount = firstNameLength - secondNameLength;
+
+console.log(
+  `The name ${firstName} is longer than the name ${secondName} by ${characterAmount} character(s)`
+);
---
+++ very easy/veryeasy.js
@@ -0,0 +1,7 @@
+var num1 = 6324;
+var num2 = 2674;
+
+answer = num1 - num2;
+console.log(
+  " The Difference Between " + num1 + " and " + num2 + " is " + answer
+);
